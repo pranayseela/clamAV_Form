603f55f75885ad3788e531d59f38d2b66a1fc999
pranay seela <pranayritvik@gmail.com>
2017-10-03 19:20:24 -0500

    Add project files.

---
+++ clamAV/Index.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Async="true" CodeBehind="Index.aspx.cs" Inherits="clamAV.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" enctype="multipart/form-data">
+        <div>
+            <asp:Label ID="lblMsg" runat="server" Text="" ForeColor="Red" Font-Bold="true"></asp:Label><br /><br />
+
+            File: <asp:FileUpload ID="flUploadFile" runat="server" />
+            <asp:Button ID="btnFileScan" runat="server" Text="Upload & Scan" OnClick="btnFileScan_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ clamAV/Index.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using nClam;
+using System.Threading.Tasks;
+
+namespace clamAV
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected async void btnFileScan_Click(object sender, EventArgs e)
+        {
+            if (flUploadFile.HasFile)
+            {
+               await Scan();
+            }
+        }
+        public async Task Scan()
+        {
+            var clam = new ClamClient("localhost", 3310);
+            var scanResult = await clam.SendAndScanFileAsync(flUploadFile.PostedFile.InputStream);//.ScanFileOnServerAsync(flUploadFile.PostedFile.FileName);  //any file you would like!
+
+            switch (scanResult.Result)
+            {
+                case ClamScanResults.Clean:
+                    lblMsg.Text = "The file is clean!";
+                    break;
+                case ClamScanResults.VirusDetected:
+                    lblMsg.Text = "Virus Found! " + string.Format("Virus name: {0}", scanResult.InfectedFiles.First().VirusName);
+
+                    break;
+                case ClamScanResults.Error:
+                    lblMsg.Text = string.Format("Woah an error occured! Error: {0}", scanResult.RawResult);
+                    break;
+            }
+        }
+    }
+}
